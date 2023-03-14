6a619d4a80b6d95949e251523b7dfbac2fd84fbb
mugisha <mugishaelvis456@gmail.com>
2023-03-14 14:59:19 +0200

    add things

--- js/javascript.js
+++ js/javascript.js
@@ -2,3 +2,14 @@ const header=document.querySelector("header");
 window.addEventListener("scroll",function(){
     header.classList.toggle("sticky",window.scrollY >10);
 });
+
+function hiddenColor(){
+    document.getElementById("changer3").style.color='#ffb81e';
+}
+function changer(){
+console.log('hello');
+}
+ const links= document.querySelectorAll('.navlist');
+ links.forEach(link=>{
+    link.addEventListener('click',changer);
+ });
